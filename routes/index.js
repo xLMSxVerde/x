e24d658e6841ae304c89642c871dc73ddda70ecc
xLMSxVerde <benedictkempkes@googlemail.com>
2016-08-02 22:42:14 +0200

    excel
    added test excel

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var router = express.Router();
 if(typeof require !== 'undefined') XLSX = require('xlsx');
-var workbook = XLSX.readFile('public/data/fm/TestCup.xlsx');
-/* DO SOMETHING WITH workbook HERE */
-var first_sheet_name = workbook.SheetNames[1];
-var address_of_cell = 'B2';
 
+//Data objects
+var navData = ["navData"];
+var fmData = ["fmData"];
+var pcData = ["pcData"];
+
+var workbook = XLSX.readFile('public/data/fm/Einzelwertung.xlsm');
+getSerieData(workbook, "fm");
+/* DO SOMETHING WITH workbook HERE */
+var first_sheet_name = workbook.SheetNames[0];
+var address_of_cell = 'B2';
 /* Get worksheet */
 var worksheet = workbook.Sheets[first_sheet_name];
-
 /* Find desired cell */
 var desired_cell = worksheet[address_of_cell];
-
 /* Get the value */
 var desired_value = desired_cell.v;
-var items = XLSX.utils.sheet_to_json(worksheet);
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+  console.log(fmData);
 });
 
 router.post('/', function(req, res, next) {
-  var navData = ["Serie1","Serie2","Serie3"];
-  var fmData = ["SerieFM"];
-  var pcData = ["SeriePC"];
   res.json({navData: navData, fmData: fmData, pcData: pcData});
 });
 
@@ -34,5 +34,29 @@ router.get('/partials/:name', function (req, res){
   var name = req.params.name;
   res.render('partials/' + name);
 });
+
+function getSerieData(data, game){
+  var newSerie = [];
+  //Serieinformation -> B2 till B*
+  var sheetinfo = data.Sheets["Information"];
+  //Standings
+  var standings = [];
+
+  //Results
+  var results = [];
+  switch (game) {
+    case "pc":
+        pcData.push(sheetinfo);
+        pcData.push(standings);
+        pcData.push(results);
+      break;
+    case "fm":
+        fmData.push(sheetinfo);
+        fmData.push(standings);
+        fmData.push(results);
+      break;
+    default:
+  }
+}
 
 module.exports = router;
